79e19b042e5938f7a6a64e8332bef892be3562ff
mauvda <mauricio.lamonatto@gmail.com>
2022-11-17 14:27:33 -0300

    Primeiro projeto de medicao de area em um circulo

--- aula.py
+++ aula.py
@@ -1,2 +1,7 @@
-#Linha local para teste
-# outro teste
+# Iniciando os Projetos do Curso de python na udemy
+pi = 3.1415
+raio = 50
+formula = pi*raio**2
+print('Se o Raio e igual a', raio,
+      '\nEntao a area total',
+      'do circulo e igual a', formula)
