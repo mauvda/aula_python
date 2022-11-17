c85f24f8b683e756b75d78f381cdc6dd780e3d65
mauvda <mauricio.lamonatto@gmail.com>
2022-11-17 17:02:22 -0300

    correcao de identação do python

--- aula.py
+++ aula.py
@@ -5,6 +5,6 @@ import math
 raio = input('Informe o raio: ')
 formula = math.pi*float(raio)**2
 if __name__ == '__main__':
-      print('Se o Raio e igual a', raio,
-            '\nEntao a area total',
-            'do circulo e igual a', "%.2f" % formula)
+    print('Se o Raio e igual a', raio,
+          '\nEntao a area total',
+          'do circulo e igual a', "%.2f" % formula)
