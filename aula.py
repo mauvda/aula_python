3abb5ea657bf7196792d3cec53014d9746fb84ec
mauvda <mauricio.lamonatto@gmail.com>
2022-11-17 14:56:44 -0300

    Ajuste de casas decimais do resultado em formula

--- aula.py
+++ aula.py
@@ -4,4 +4,4 @@ raio = 25
 formula = pi*raio**2
 print('Se o Raio e igual a', raio,
       '\nEntao a area total',
-      'do circulo e igual a', formula)
+      'do circulo e igual a', "%.2f" % formula)
