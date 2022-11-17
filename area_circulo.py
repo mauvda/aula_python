2e60fbb04cecc42ec4d5295b06d5ecb0b70f9f7a
mauvda <mauricio.lamonatto@gmail.com>
2022-11-17 17:37:38 -0300

    Removido entrada do usuario e configurado entrada via console com import sys

--- area_circulo.py
+++ area_circulo.py
@@ -1,6 +1,7 @@
 #! python
 # from math import pi
 import math
+import sys
 
 
 def formula(raio):
@@ -8,6 +9,6 @@ def formula(raio):
 
 
 if __name__ == '__main__':
-    raio = input('Informe o raio: ')
+    raio = sys.argv[1]
     area = formula(raio)
     print('Se o raio é', raio, 'entao a area é', "%.2f" % area)
