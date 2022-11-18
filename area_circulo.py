ccd0f526072d6f1ab57d84390eb3dd1707281d44
mauvda <mauricio.lamonatto@gmail.com>
2022-11-18 10:54:35 -0300

    Adicionar função help para simplificar chamada de tratativa de erro

--- area_circulo.py
+++ area_circulo.py
@@ -8,10 +8,15 @@ def formula(raio):
     return math.pi*float(raio)**2
 
 
+def help():
+    print('É necessário informar o Raio do circulo',
+          '\nSintaxe: python', sys.argv[0], '<Raio>')
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('É necessário informar o Raio do circulo',
-              '\nSintaxe: python', sys.argv[0], '<Raio>')
+        help()
+        # sys.exit(1)
     else:
         raio = sys.argv[1]
         area = formula(raio)
