0ea67a5d1b9eef4233136162d936f296aef783f2
mauvda <mauricio.lamonatto@gmail.com>
2022-11-23 17:47:44 -0300

    Alterando while por FOR

--- fibonacci_projeto/fibonacci_v2.py
+++ fibonacci_projeto/fibonacci_v2.py
@@ -4,14 +4,12 @@
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
 def fibonacci(quantidade):
     resultado = [0, 1]
-    while True:
+    for _ in range(quantidade - 2):
         resultado.append(sum(resultado[-2:]))
-        if len(resultado) == quantidade:
-            break
     return resultado
 
 
 if __name__ == '__main__':
     # Listar os 301 primeiros numeros da sequencia de fibonacci
-    for fib in fibonacci(301):
+    for fib in fibonacci(20):
         print(fib, end=', ')
