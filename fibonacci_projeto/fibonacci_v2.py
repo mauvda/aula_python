4ac131a949a4d425f2efa9936becb5152d2b18c0
mauvda <mauricio.lamonatto@gmail.com>
2022-11-23 17:08:01 -0300

    Implementando o metodo sum() para otimizar soma

--- fibonacci_projeto/fibonacci_v2.py
+++ fibonacci_projeto/fibonacci_v2.py
@@ -5,10 +5,11 @@
 def fibonacci(limite):
     resultado = [0, 1]
     while resultado[-1] < limite:
-        resultado.append(resultado[-1] + resultado[-2])
+        # resultado.append(resultado[-1] + resultado[-2])
+        resultado.append(sum(resultado[-2:]))
     return resultado
 
 
 if __name__ == '__main__':
-    for fib in fibonacci(1000000000000):
+    for fib in fibonacci(1000000000000000000000000):
         print(fib, end=', ')
