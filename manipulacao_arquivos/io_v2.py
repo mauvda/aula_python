266b73b664a9b2f4f2a7595bf18aef2d87cddf96
mauvda <mauricio.lamonatto@gmail.com>
2022-11-24 10:25:52 -0300

    Criação de arquivo pessoas.txt por meio de codigo

--- manipulacao_arquivos/io_v2.py
+++ manipulacao_arquivos/io_v2.py
@@ -1,7 +1,9 @@
 #! python
 with open('pessoas.csv') as arquivo:
-    for run in arquivo:
-        print('Nome: {}, Idade {}'.format(*run.strip().split(',')))
+    with open('pessoas.txt', 'w') as saida:
+        for run in arquivo:
+            pessoa = run.strip().split(',')
+            print('Nome: {}, Idade {}'.format(*pessoa), file=saida)
 
 if arquivo.closed:
     print('Arquivos foi fechado')
