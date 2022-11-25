a762da84d23689700639b645cbd56d6ec53080f5
mauvda <mauricio.lamonatto@gmail.com>
2022-11-25 14:56:59 -0300

    Criação de coleta de dados de desafio-ibge.csv para ibge.txt

---
+++ manipulacao_arquivos/io_csv.py
@@ -0,0 +1,14 @@
+#! python
+# - * - Coding : ISO-8859-1 - * -
+import csv
+
+try:
+    with open('desafio-ibge.csv') as arquivo:
+        with open('ibge.txt', 'w') as escrita:
+            for run in csv.reader(arquivo):
+                print('Nome de Origem: {} e Nome de destino: {}'.format(
+                      *run[3].strip().split(','),
+                      *run[8].strip().split(',')), file=escrita)
+finally:
+    if arquivo.closed and escrita.closed:
+        print('Arquivo fechado')
